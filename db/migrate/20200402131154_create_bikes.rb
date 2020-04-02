ee7f7caf31229e574abec15abcaade50b74d6e0b
tyamap <tom333my@outlook.com>
2020-04-02 23:18:35 +0900

    [mod] テーブル設定

--- db/migrate/20200402131154_create_bikes.rb
+++ db/migrate/20200402131154_create_bikes.rb
@@ -1,8 +1,14 @@
+# 自転車の在庫テーブルを設定
 class CreateBikes < ActiveRecord::Migration[6.0]
   def change
     create_table :bikes do |t|
+      t.references    :brand,          null: false       # brandsテーブルのFK 
+      t.string        :serial_number,   null: false       # シリアルナンバー
+      t.datetime      :sold_at                            # 売却日 
 
       t.timestamps
     end
+    add_index :bikes, 'serial_number', unique: true
+    add_index :bikes, [ :brand_id, :sold_at ]
   end
 end
