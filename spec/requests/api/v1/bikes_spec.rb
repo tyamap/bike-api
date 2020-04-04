149102c3fa6a999c5619e4dc0d025715b30056df
tyamap <tom333my@outlook.com>
2020-04-04 22:06:59 +0900

    [add] 自転車情報取得機能のRspec

--- spec/requests/api/v1/bikes_spec.rb
+++ spec/requests/api/v1/bikes_spec.rb
@@ -7,3 +7,18 @@ describe 'NewBikeApi' do
     expect(response.status).to eq(201)
   end
 end
+
+describe 'ShowBikesApi' do
+  before do
+    @brand = FactoryBot.create(:brand)
+  end
+  example '要求ブランド名の自転車を一覧表示する' do
+    valid_params = { brand_name: @brand.name }
+    FactoryBot.create_list(:bike, 10, brand: @brand)
+    get '/api/v1/bikes', params: valid_params
+    json = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(json.length).to eq(10)
+  end
+end
