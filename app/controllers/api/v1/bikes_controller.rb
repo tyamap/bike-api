fa5405cbd3cdda7807735a2945e6e8d782605cf8
tyamap <tom333my@outlook.com>
2020-04-06 22:44:32 +0900

    [mod] 軽微な修正

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::BikesController < ApplicationController
   before_action :set_bike, only: [:update]
 
   # GET api/v1/bikes
+  # 自転車情報取得API
   def index
     bikes = @brand.bikes.all
     bikes = bikes.includes(:brand)
@@ -12,6 +13,7 @@ class Api::V1::BikesController < ApplicationController
   end
 
   # POST /api/v1/bikes
+  # 自転車情報登録API
   def create
     bike = Bike.new(brand_id: @brand.id, serial_number: params[:serial_number])
     if bike.save
@@ -22,6 +24,7 @@ class Api::V1::BikesController < ApplicationController
   end
 
   # PUT /api/v1/bikes
+  # 自転車売却API
   def update
     @bike.sold_at = Time.zone.now
     if @bike.save
@@ -33,18 +36,21 @@ class Api::V1::BikesController < ApplicationController
 
   private
 
+  # ブランド名からブランドインスタンスを取得
   def set_brand
     @brand = Brand.find_by(name: params[:brand_name])
-    render status: :not_found, json: { errors: :not_found } if @brand.nil?
+    render status: :not_found, json: { errors: { brand_name: ['not found'] } } if @brand.nil?
   end
 
+  # ブランド名からブランドインスタンスを取得。該当ブランドがなければ新規に登録
   def create_or_set_brand
     @brand = Brand.find_by(name: params[:brand_name])
     @brand = Brand.create(name: params[:brand_name]) if @brand.nil?
   end
 
+  # シリアルナンバーから自転車インスタンスを取得
   def set_bike
     @bike = Bike.find_by(serial_number: params[:serial_number])
-    render status: :not_found, json: { errors: :not_found } if @bike.nil?
+    render status: :not_found, json: { errors: { serial_number: ['not found'] } } if @bike.nil?
   end
 end
