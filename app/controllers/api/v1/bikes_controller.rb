b3334213c8b9a60bdf4e95f042b0926d2178cbfc
tyamap <tom333my@outlook.com>
2020-04-06 22:12:24 +0900

    [mod] Controllerのリファクタリング

--- app/controllers/api/v1/bikes_controller.rb
+++ app/controllers/api/v1/bikes_controller.rb
@@ -1,28 +1,19 @@
 # 自転車管理コントローラー
 class Api::V1::BikesController < ApplicationController
+  before_action :set_brand, only: [:index]
+  before_action :create_or_set_brand, only: [:create]
+  before_action :set_bike, only: [:update]
+
   # GET api/v1/bikes
   def index
-    brand = get_brand_by_name(params[:brand_name])
-    # TODO: nilだった場合のエラーハンドリングを親クラスに定義して共通化
-    if brand.present?
-      bikes = brand.bikes.all
-      bikes = bikes.includes(:brand)
-      render status: :ok, json: { data: bikes }
-    else
-      render status: :not_found, json: { errors: :not_found }
-    end
+    bikes = @brand.bikes.all
+    bikes = bikes.includes(:brand)
+    render status: :ok, json: { data: bikes }
   end
 
   # POST /api/v1/bikes
   def create
-    brand = Brand.find_by(name: params[:brand_name])
-    # TODO: 以下のロジックを分離したほうが良い？
-    brand_id = if brand.nil?
-                 Brand.create(name: params[:brand_name]).id
-               else
-                 brand.id
-               end
-    bike = Bike.new(brand_id: brand_id, serial_number: params[:serial_number])
+    bike = Bike.new(brand_id: @brand.id, serial_number: params[:serial_number])
     if bike.save
       render status: :created, json: { data: bike }
     else
@@ -32,22 +23,28 @@ class Api::V1::BikesController < ApplicationController
 
   # PUT /api/v1/bikes
   def update
-    bike = Bike.find_by(serial_number: params[:serial_number])
-    if bike.present?
-      bike.sold_at = Time.zone.now
-      if bike.save
-        render status: :accepted, json: { data: bike }
-      else
-        render status: :unprocessable_entity, json: { errors: bike.errors }
-      end
+    @bike.sold_at = Time.zone.now
+    if @bike.save
+      render status: :accepted, json: { data: @bike }
     else
-      render status: :not_found, json: { errors: :not_found }
+      render status: :unprocessable_entity, json: { errors: @bike.errors }
     end
   end
 
   private
 
-  def get_brand_by_name(name)
-    Brand.find_by(name: name)
+  def set_brand
+    @brand = Brand.find_by(name: params[:brand_name])
+    render status: :not_found, json: { errors: :not_found } if @brand.nil?
+  end
+
+  def create_or_set_brand
+    @brand = Brand.find_by(name: params[:brand_name])
+    @brand = Brand.create(name: params[:brand_name]) if @brand.nil?
+  end
+
+  def set_bike
+    @bike = Bike.find_by(serial_number: params[:serial_number])
+    render status: :not_found, json: { errors: :not_found } if @bike.nil?
   end
 end
